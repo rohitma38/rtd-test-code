c9653386dfc55626df4e885487aa2e861ef30d3d
Rohit M A <rohitma38@gmail.com>
2022-01-20 19:51:57 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,8 @@ author = 'rohitma38'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
 ]
 
+autodoc_mock_imports = ["soundfile"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
